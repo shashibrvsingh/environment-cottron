f88d7f63ab0b7e2c25186114303cdfc182ac485f
1ShashiBhushanSingh4 <shashibhushansingh001@gmail.com>
2022-05-17 11:14:40 +0530

    commit

--- js/front-slider.js
+++ js/front-slider.js
@@ -28,3 +28,7 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+// ************************************
+// animation-opening
+// ************************************
